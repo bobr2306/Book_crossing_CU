4162b8da8ff6530988a56dbe0cb7dae1bbeaa3cd
Konst <k.popov@edu.centraluniversity.ru>
2025-05-06 19:28:22 +0300

    added models.py and frontend structure

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
+from backend.src.models import User
 
+# Books
 def create_book(db: Session, book: dict):
     pass
 
@@ -15,4 +17,57 @@ def update_book(db: Session, book: dict):
 def delete_book(db: Session, book: dict):
     pass
 
+# Users
+def create_user(db: Session, user: dict):
+    pass
 
+def get_users():
+    pass
+
+def get_user():
+    pass
+
+def update_user(db: Session, user: dict):
+    pass
+
+def delete_user(db: Session, user: dict):
+    pass
+
+# Reviews
+def create_review(db: Session, review: dict):
+    pass
+
+def get_reviews():
+    pass
+
+def get_review():
+    pass
+
+def update_review(db: Session, review: dict):
+    pass
+
+def delete_review(db: Session, review: dict):
+    pass
+
+# Collections
+def create_collection(db: Session, collection: dict):
+    pass
+
+def get_collections():
+    pass
+
+def get_collection():
+    pass
+
+def update_collection(db: Session, collection: dict):
+    pass
+
+def delete_collection(db: Session, collection: dict):
+    pass
+
+# Transactions
+def create_transaction(db: Session, transaction: dict):
+    pass
+
+def get_transaction():
+    pass
